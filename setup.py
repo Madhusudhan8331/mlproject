4505d6230a2bf7768da90bc28ce3643706955f3c
Madhusudhan8331 <madhunagidi.6872@gmail.com>
2025-10-23 22:47:53 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='madhu',
+author_email='madhunagidi.6872@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+)
